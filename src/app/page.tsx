f72feb8bd53ec2a7e4d1b8055ba27216f960f33c
SubashMallireddy <subash@tonic.com.au>
2025-03-05 16:46:53 +1100

    add three columsn to the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <section className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h2>The Amazing Data Page</h2>
-    </section>
+      <div className="grid grid-cols-3">
+        <div>Column - 1 </div>
+        <div>Column - 2 </div>
+        <div>Column - 3 </div>
+      </div>
+    </div>
   );
 }
